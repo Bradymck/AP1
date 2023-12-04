70327bd7609eb4e22e4df8e1e8317decec578a35
Bradymck <4667076+Bradymck@users.noreply.github.com>
2023-12-04 06:04:12 +0000

    Fixed Database connections

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-https://github.com/district0x/discord-py-bots/tree/master/AquaPrime# District0x Discord Python Chatbots
+# **Aqua Prime: Mastering the Economic Escape Room in the Satirical Metaverse**
 
-This repository contains the source code for various Python chatbots designed to enhance the user experience on the District0x Discord server. These bots provide utility, facilitate interactions, and offer unique features tailored to the District0x community.
+Welcome to Aqua Prime, where economic strategy meets digital satire in an indie gaming experience unlike any other. As you delve into the world of Aqua Prime, you'll find yourself at the intersection of a Minimum Viable Metaverse, blending elements from Tabletop Role-Playing Games (TTRPGs), Massively Multiplayer Online (MMO) games, Non-Fungible Tokens (NFTs), Artificial Intelligence (AI), and Decentralized Autonomous Organizations (DAOs). The heart of this journey is the economic escape room, a unique challenge that blends virtual and real-world economics.
 
-## Bots Overview:
+### **The Economic Escape Room: Your Strategic Path**
 
-### 1. EthlanceGPT
-The EthlanceGPT Discord Chatbot assists users in locating job opportunities or connecting with freelance workers who possess the appropriate skills for a specific job.
+Aqua Prime centers around an economic escape room set within a 'sand dollar' economy. This simulated economy is your playground for strategy and ingenuity, where the goal is to transition into a real, tangible crypto economy. It's not just about gathering resources; it's about mastering economic principles, making strategic alliances, and manipulating market dynamics. This transition from virtual to real economies is the essence of your victory.
 
-### 2. NamebazaarBot
-NamebazaarBot facilitates Ethereum Name Service (ENS) name trading, enabling users to trade or register ENS names directly within Discord.
+### **TTRPGMMONFTAIDAO: The Satirical Edge**
 
-### 3. Aqua Prime Grumpy Cat AI
-The Aqua Prime Grumpy Cat AI is a component of the Aqua Prime game—a Discord-based Tabletop Role-Playing Game (TTRPG) that combines economic strategy, education about cryptocurrency, and interactive storytelling. The Grumpy Cat AI serves as the game's Dungeon Master, providing witty and humorous responses in the character of Grumpy Cat to players' interactions and inquiries. The AI utilizes OpenAI's GPT-3.5 engine to generate contextually relevant and engaging responses.
+The game's satirical soul is encapsulated in its acronym, TTRPGMMONFTAIDAO. This playful take mocks the complexity of today's digital and economic trends. As players, you'll interact with ARIs and utilize ERC-6551 NFTs, which act as gateways to new realms within the game. This satirical layer invites players to engage critically with the burgeoning concepts of AI and blockchain, adding depth to the gameplay.
 
-### 4. MemeFactory Bot
-The MemeFactory bot is designed to enhance the user experience on Discord for MemeFactory enthusiasts. It provides a range of functionalities from listing memes, fetching meme details, to facilitating purchases on OpenSea. Key features include listing MemeFactory memes, fetching details of specific memes, viewing cheapest listings for memes, buying memes from OpenSea, viewing offers/bids for memes, linking/unlinking Ethereum wallet, selling memes on OpenSea, viewing owned memes, publicly displaying meme collection, and viewing linked wallet details.
+### **Faction Play: Building Your Community**
 
-(Additional bots can be listed here as you add them to the repository.)
+In Aqua Prime, players are encouraged to form or join factions, like the Meme Factory or the Moonstone Maverick. Each faction, represented as a ship, has its unique economy and backstory, enhancing the game's lore. Web3 communities can apply to be part of the game, creating an interconnected network of players and narratives. Your faction’s decisions and alliances will shape the game's economic and social landscape.
 
-## Getting Started
+### **The Minimum Viable Metaverse: Where Strategy Meets Satire**
 
-To get started with any of the bots, clone this repository, install the required dependencies, and follow the individual setup instructions provided in each bot's directory.
+This concept is a playful yet insightful commentary on the digital age. In the Minimum Viable Metaverse, every decision impacts the collective narrative and the game's economic ecosystem. Here, strategy and satire converge, offering a rich, multi-layered gaming experience.
 
-## Contributions
+### **How to Win: The On-Chain Triumph**
 
-Contributions are welcome! If you have suggestions, bug reports, or enhancements, feel free to open an issue or submit a pull request.
+Winning in Aqua Prime is unique. It's about successfully transitioning your virtual assets and strategies into the real-world blockchain. This means taking the skills, alliances, and resources you've developed in the game and applying them to actual crypto-economic scenarios. Winning is about making this transition effectively, moving from the game's economic sandbox into real-world blockchain interactions. It's a journey that is both challenging and exhilarating, pushing players to explore the boundaries of digital economies.
+
+### **Embark on Your Journey**
+
+Aqua Prime is more than just a game. It's an adventure into the heart of a digital world, where strategy, satire, and economic principles intertwine. Are you ready to navigate these waters, master the economic escape room, and emerge victorious in the blockchain realm? Join us in Aqua Prime and shape the future of this unique indie adventure. 🌐🎲🚀💼🎭
--- gameCommands.py
+++ gameCommands.py
@@ -9,166 +9,183 @@ from discord.ext import commands
 
 
 class CommandsCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
 
+  def __init__(self, bot):
+    self.bot = bot
 
-    @commands.command(name='attack', brief="Rolls for an attack with a d20 and modifier")
-    @commands.cooldown(1, 86400, commands.BucketType.user)  # 24-hour cooldown
-    async def attack(self, ctx, user: discord.Member = None):
-        await self.perform_attack(ctx, user)
+  @commands.command(name='attack',
+                    brief="Rolls for an attack with a d20 and modifier")
+  @commands.cooldown(1, 86400, commands.BucketType.user)  # 24-hour cooldown
+  async def attack(self, ctx, user: discord.Member = None):
+    await self.perform_attack(ctx, user)
 
-    async def perform_attack(self, ctx, user: discord.Member = None):
-        print("\033[31mPerforming attack...\033[0m")
-        roll = random.randint(1, 20)
-        total = roll
+  async def perform_attack(self, ctx, user: discord.Member = None):
+    print("\033[31mPerforming attack...\033[0m")
+    roll = random.randint(1, 20)
+    total = roll
 
-        member = ctx.guild.get_member(ctx.author.id)
-        if any(role.id == 1081109959292497928 for role in member.roles):
-            roll2 = random.randint(1, 20)
-            total += roll2
-        else:
-            pass
+    member = ctx.guild.get_member(ctx.author.id)
+    if any(role.id == 1081109959292497928 for role in member.roles):
+      roll2 = random.randint(1, 20)
+      total += roll2
+    else:
+      pass
 
-        target_user_mention = f"is attacking {user.mention}" if user else "is attacking"
-        embed = discord.Embed(colour=0x00b0f4, timestamp=datetime.now())
-        embed.set_author(name="Attack Roll", icon_url="https://images-ext-1.discordapp.net/external/AkSGsZL11_KdXNhA2QqQs3OusLy8-_u65pmdhYZ3mzU/https/images.emojiterra.com/google/android-12l/512px/2694.png")
-        embed.add_field(
-            name="", value=f"{ctx.author.mention} {target_user_mention}", inline=False)
-        embed.add_field(
-            name="`  🎲  `", value=f"`{roll} ⚔`", inline=True)
-        if any(role.id == 1081109959292497928 for role in member.roles):
-            embed.add_field(
-                name="`  🎲  `", value=f"`{roll2} ⚔`", inline=True)
-        embed.add_field(name="`Total 💥`", value=f"{total} ⚔", inline=False)
-        embed.set_thumbnail(
-            url="https://thumbs.gfycat.com/CompleteCornyGalago-max-1mb.gif")
-        embed.set_footer(
-            text="Aqua Prime", icon_url="https://media.discordapp.net/attachments/1094443050098511922/1142958547353731104/ezgif-5-15676eab6f.gif")
+    target_user_mention = f"is attacking {user.mention}" if user else "is attacking"
+    embed = discord.Embed(colour=0x00b0f4, timestamp=datetime.now())
+    embed.set_author(
+        name="Attack Roll",
+        icon_url=
+        "https://images-ext-1.discordapp.net/external/AkSGsZL11_KdXNhA2QqQs3OusLy8-_u65pmdhYZ3mzU/https/images.emojiterra.com/google/android-12l/512px/2694.png"
+    )
+    embed.add_field(name="",
+                    value=f"{ctx.author.mention} {target_user_mention}",
+                    inline=False)
+    embed.add_field(name="`  🎲  `", value=f"`{roll} ⚔`", inline=True)
+    if any(role.id == 1081109959292497928 for role in member.roles):
+      embed.add_field(name="`  🎲  `", value=f"`{roll2} ⚔`", inline=True)
+    embed.add_field(name="`Total 💥`", value=f"{total} ⚔", inline=False)
+    embed.set_thumbnail(
+        url="https://thumbs.gfycat.com/CompleteCornyGalago-max-1mb.gif")
+    embed.set_footer(
+        text="Aqua Prime",
+        icon_url=
+        "https://media.discordapp.net/attachments/1094443050098511922/1142958547353731104/ezgif-5-15676eab6f.gif"
+    )
 
-        await ctx.send(embed=embed)
+    await ctx.send(embed=embed)
 
-    @commands.command(name='shoot', brief="Shoots a user with a gun")
-    async def shoot(self, ctx, user: discord.Member):
-        if user.id == self.bot.user.id:
-            await ctx.send("Nice try. I can't be killed. I'm a cat with 9 lives, and I'm a meme. Not to mention I somehow handle gun fire in the game so I'm not even gonna bother with that.")
-            return
+  @commands.command(name='shoot', brief="Shoots a user with a gun")
+  async def shoot(self, ctx, user: discord.Member):
+    if user.id == self.bot.user.id:
+      await ctx.send(
+          "Nice try. I can't be killed. I'm a meme. Not to mention I somehow handle gun fire in the game so I'm not even gonna bother with explaininng that. 🤣"
+      )
+      return
 
-        # Check if the target user is the boss
-        boss_role = discord.utils.get(ctx.guild.roles, name='Boss')
-        if boss_role in user.roles:
-            await ctx.send("You can't shoot the boss, they're too powerful!")
-            return
+    # Check if the target user is the boss
+    boss_role = discord.utils.get(ctx.guild.roles, name='Boss')
+    if boss_role in user.roles:
+      await ctx.send("You can't shoot the boss, they're too powerful!")
+      return
 
-        print("\033[34mCommand executed\033[0m")
+    print("\033[34mCommand executed\033[0m")
 
-        # Check if the user has the Special Role or ID 🔫
-        member = ctx.guild.get_member(ctx.author.id)
-        if any(role.id in [1076926977518346412, 1081417524043857943] for role in member.roles):
+    # Check if the user has the Special Role or ID 🔫
+    member = ctx.guild.get_member(ctx.author.id)
+    if any(role.id in [1081109959162466319, 1081417524043857943]
+           for role in member.roles):
 
-            role_to_remove1 = discord.utils.get(
-                ctx.guild.roles, id=1059989387875729438)
-            role_to_remove2 = discord.utils.get(
-                ctx.guild.roles, id=1076926977518346412)
-            role_to_add = discord.utils.get(
-                ctx.guild.roles, id=1075838775307030598)
+      role_to_remove1 = discord.utils.get(ctx.guild.roles,
+                                          id=1081109959309279264)
+      role_to_remove2 = discord.utils.get(ctx.guild.roles,
+                                          id=1081109959162466319)
+      role_to_add = discord.utils.get(ctx.guild.roles, id=1081109959309279270)
 
-            await asyncio.sleep(10)  # Add a 10-second delay
-            await user.remove_roles(role_to_remove1, role_to_remove2)
-            await member.remove_roles(role_to_remove2)
-            await user.add_roles(role_to_add)
-            print("\033[34mRole added\033[0m")
-            await ctx.send(f"{ctx.author.mention} just shot and killed {user.mention} 💥🔫. But... their lame little 3D printed gun broke in the process.")
+      await asyncio.sleep(10)  # Add a 10-second delay
+      await user.remove_roles(role_to_remove1, role_to_remove2)
+      await member.remove_roles(role_to_remove2)
+      await user.add_roles(role_to_add)
+      print("\033[34mRole added\033[0m")
+      await ctx.send(
+          f"{ctx.author.mention} just shot and killed {user.mention} 💥🔫. But... their lame little 3D printed gun broke in the process."
+      )
 
-        else:
-            await ctx.send("You need to buy a gun first. 🔫")
+    else:
+      await ctx.send("You need to buy a gun first. 🔫")
 
-    @commands.command(name='res', brief="Adds a role to a user")
-    async def res(self, ctx, user: discord.Member):
-        command_user = ctx.message.author
-        if any(role.id in [1080938165625442314] for role in command_user.roles):
-            command_user_role = discord.utils.get(
-                ctx.guild.roles, id=1080938165625442314)
-            target_user_role = discord.utils.get(
-                ctx.guild.roles, id=1075838775307030598)
-            new_role = discord.utils.get(
-                ctx.guild.roles, id=1059989387875729438)
+  @commands.command(name='res', brief="Adds a role to a user")
+  async def res(self, ctx, user: discord.Member):
+    command_user = ctx.message.author
+    if any(role.id in [1081109959124729867] for role in command_user.roles):
+      command_user_role = discord.utils.get(ctx.guild.roles,
+                                            id=1081109959124729867)
+      target_user_role = discord.utils.get(ctx.guild.roles, 
+                                           id=1081109959309279270)
+      new_role = discord.utils.get(ctx.guild.roles, id=1081109959309279264)
 
-            await command_user.remove_roles(command_user_role)
-            await user.remove_roles(target_user_role)
-            await user.add_roles(new_role)
-            await ctx.send(f"{ctx.author.mention} just resurrected {user.mention}")
+      await command_user.remove_roles(command_user_role)
+      await user.remove_roles(target_user_role)
+      await user.add_roles(new_role)
+      await ctx.send(f"{ctx.author.mention} just resurrected {user.mention}")
 
-        else:
-            await ctx.send("You don't have the permission to use this command.")
+    else:
+      await ctx.send("You don't have the permission to use this command.")
 
-    @commands.command(name='dice', brief="Rolls two six-sided dice")
-    async def roll(self, ctx):
-        roll1 = random.randint(1, 6)
-        roll2 = random.randint(1, 6)
-        total = roll1 + roll2
+  @commands.command(name='dice', brief="Rolls two six-sided dice")
+  async def roll(self, ctx):
+    roll1 = random.randint(1, 6)
+    roll2 = random.randint(1, 6)
+    total = roll1 + roll2
 
-        output = f"**{roll1}** :game_die:   **{roll2}** :game_die:   =   **{total}** :tada:"
-        embed = discord.Embed(title="Dice Roll", color=discord.Color.blue())
-        embed.add_field(name="Results", value=output, inline=False)
-        embed.description = f"{ctx.author.mention} rolled the dice! 🎲"
-        embed.set_thumbnail(
-            url="https://thumbs.gfycat.com/CompleteCornyGalago-max-1mb.gif")
-        await ctx.send(embed=embed)
+    output = f"**{roll1}** :game_die:   **{roll2}** :game_die:   =   **{total}** :tada:"
+    embed = discord.Embed(title="Dice Roll", color=discord.Color.blue())
+    embed.add_field(name="Results", value=output, inline=False)
+    embed.description = f"{ctx.author.mention} rolled the dice! 🎲"
+    embed.set_thumbnail(
+        url="https://thumbs.gfycat.com/CompleteCornyGalago-max-1mb.gif")
+    await ctx.send(embed=embed)
 
-    @commands.command(name='spell', brief="Casts a random spell from the spell book")
-    async def spell(self, ctx):
-        spell_book = discord.utils.get(ctx.guild.roles, name='Spell Book')
-        if spell_book not in ctx.message.author.roles:
-            await ctx.send("You need to buy the Spell Book from the store first!")
-            return
+  @commands.command(name='spell',
+                    brief="Casts a random spell from the spell book")
+  async def spell(self, ctx):
+    spell_book = discord.utils.get(ctx.guild.roles, name='Spell Book')
+    if spell_book not in ctx.message.author.roles:
+      await ctx.send("You need to buy the Spell Book from the store first!")
+      return
 
-        user_id = ctx.message.author.id
-        current_time = datetime.datetime.now()
-        if user_id in last_execution:
-            lasttime = last_execution[user_id]
-            difference = current_time - lasttime
-            if difference.days < 1:
-                await ctx.send(f"This command is on cooldown. Please try again in {1 - difference.days} days.")
-                return
+    user_id = ctx.message.author.id
+    current_time = datetime.datetime.now()
+    if user_id in last_execution:
+      lasttime = last_execution[user_id]
+      difference = current_time - lasttime
+      if difference.days < 1:
+        await ctx.send(
+            f"This command is on cooldown. Please try again in {1 - difference.days} days."
+        )
+        return
 
-        last_execution[user_id] = current_time
-        result = random.randint(1, len(spells.effects))
-        emoji = '🎲'
-        effect = spells.effects.get(result, "No effect.")
-        if result == 1:
-            role = discord.utils.get(ctx.guild.roles, name='🐸')
-            original_nick = ctx.author.display_name
-            await ctx.author.edit(nick=original_nick + ' 🐸')
-            await ctx.author.add_roles(role)
-            await ctx.author.edit(mute=True)
-            await asyncio.sleep(28800)
-            await ctx.author.edit(mute=False)
-            await ctx.author.remove_roles(role)
-            await ctx.send(f"{ctx.author.mention} cast a spell! You've been turned into a 🐸 for 8 hours! Enjoy!")
-        else:
-            await ctx.send(f"{ctx.author.mention} cast a spell! {effect} {emoji}")
+    last_execution[user_id] = current_time
+    result = random.randint(1, len(spells.effects))
+    emoji = '🎲'
+    effect = spells.effects.get(result, "No effect.")
+    if result == 1:
+      role = discord.utils.get(ctx.guild.roles, name='🐸')
+      original_nick = ctx.author.display_name
+      await ctx.author.edit(nick=original_nick + ' 🐸')
+      await ctx.author.add_roles(role)
+      await ctx.author.edit(mute=True)
+      await asyncio.sleep(28800)
+      await ctx.author.edit(mute=False)
+      await ctx.author.remove_roles(role)
+      await ctx.send(
+          f"{ctx.author.mention} cast a spell! You've been turned into a 🐸 for 8 hours! Enjoy!"
+      )
+    else:
+      await ctx.send(f"{ctx.author.mention} cast a spell! {effect} {emoji}")
 
 
 @commands.Cog.listener()
 async def on_command_error(self, ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        # Convert the cooldown time from seconds to hours and minutes
-        hours = int(error.retry_after // 3600)
-        minutes = int((error.retry_after % 3600) // 60)
+  if isinstance(error, commands.CommandOnCooldown):
+    # Convert the cooldown time from seconds to hours and minutes
+    hours = int(error.retry_after // 3600)
+    minutes = int((error.retry_after % 3600) // 60)
 
-        # Construct the cooldown message
-        if hours > 0:
-            cooldown_msg = f"You're on cooldown! Please wait {hours} hour(s) and {minutes} minute(s) before using this command again."
-        else:
-            cooldown_msg = f"You're on cooldown! Please wait {minutes} minute(s) before using this command again."
-
-        await ctx.send(cooldown_msg)
-        return  # This will suppress the default cooldown message
+    # Construct the cooldown message
+    if hours > 0:
+      cooldown_msg = f"You're on cooldown! Please wait {hours} hour(s) and {minutes} minute(s) before using this command again."
     else:
-        print(f"\033[34mError occurred: {error}\033[0m")
+      cooldown_msg = f"You're on cooldown! Please wait {minutes} minute(s) before using this command again."
+
+    await ctx.send(cooldown_msg)
+    return  # This will suppress the default cooldown message
+  else:
+    print(f"\033[34mError occurred: {error}\033[0m")
 
 
 async def setup(bot):
-    await bot.add_cog(CommandsCog(bot))
+  await bot.add_cog(CommandsCog(bot))
+
+
 print("Commands cog is ready.")
